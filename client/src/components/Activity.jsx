21b643f6b4b6134872da0d618b2d15f8f5ebeccd
kp-pham <170148118+kp-pham@users.noreply.github.com>
2026-01-01 22:34:21 -0800

    refactor: manage activity in activity component
    Prevent prop drilling when passing activity state from chat panel.

--- client/src/components/Activity.jsx
+++ client/src/components/Activity.jsx
@@ -1,10 +1,48 @@
+import { useEffect, useState, useRef} from 'react'
+import { useSocket } from '../contexts/hooks.js'
 import '../styles/Activity.css'
 
-function Activity({ activity }) {
+function Activity() {
+  const timerRef = useRef(null)
+  const [activity, setActivity] = useState(false)
+  const socket = useSocket()
+
+  useEffect(() => {
+    function handleActivity() {
+      setActivity(true)
+
+      if (timerRef.current)
+          clearTimeout(timerRef.current)
+
+      timerRef.current = setTimeout(() => {
+        setActivity(false)
+      }, 7000)
+    }
+
+    socket.on('activity', handleActivity)  
+      
+    return () => {
+      socket.off('activity', handleActivity)
+
+      if (timerRef.current)
+        clearTimeout(timerRef.current)
+    }
+  }, [socket])
+
+  useEffect(() => {
+    function handleMessage() {
+      setActivity(false)
+    }
+
+    socket.on('message', handleMessage)
+
+    return () => socket.off('message', handleMessage)
+  })
+
   return (
     <div 
       style={{ visibility: activity ? "visible" : "hidden" }}
-      className="activity"
+      id="activity"
     >
       Opponent is typing...
     </div>
--- client/src/components/ChatPanel.jsx
+++ client/src/components/ChatPanel.jsx
@@ -1,51 +1,13 @@
-import { useEffect, useRef, useState } from 'react'
-import { useSocket } from '../contexts/hooks.js'
 import Activity from './Activity.jsx'
 import ChatForm from './ChatForm.jsx'
 import ChatLog from './ChatLog.jsx'
 import '../styles/ChatPanel.css'
 
 function ChatPanel() {
-  const timerRef = useRef(null)
-  const [activity, setActivity] = useState(false)
-  const socket = useSocket()
-
-  useEffect(() => {
-    function handleActivity() {
-      setActivity(true)
-
-      if (timerRef.current)
-          clearTimeout(timerRef.current)
-
-      timerRef.current = setTimeout(() => {
-        setActivity(false)
-      }, 7000)
-    }
-
-    socket.on('activity', handleActivity)  
-      
-    return () => {
-      socket.off('activity', handleActivity)
-
-      if (timerRef.current)
-        clearTimeout(timerRef.current)
-    }
-  }, [socket])
-
-  useEffect(() => {
-    function handleMessage() {
-      setActivity(false)
-    }
-
-    socket.on('message', handleMessage)
-
-    return () => socket.off('message', handleMessage)
-  })
-
   return (
     <div id="chat-panel">
       <ChatLog></ChatLog>
-      <Activity activity={activity}></Activity>
+      <Activity></Activity>
       <ChatForm></ChatForm>
     </div>
   )
