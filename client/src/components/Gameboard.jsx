a3f67a8c3dfbd7923feb573712f0c5fca8cf29f7
kp-pham <170148118+kp-pham@users.noreply.github.com>
2026-01-02 09:04:06 -0800

    feat: create cards for gameboard
    Use buttons as cards to improve user experience with interactive elements.

--- client/src/components/Gameboard.jsx
+++ client/src/components/Gameboard.jsx
@@ -1,7 +1,14 @@
+import Card from './Card.jsx'
 import '../styles/Gameboard.css'
 
+const cards = ['Arrays', 'Stacks', 'Queues', 'Deques', 'Linked Lists', 'Binary Search Trees', 'Red-Black Trees', 'Preorder Traversal', 'Inorder Traversal', 'Postorder Traversal', 'Level-order Traversal', 'Skip Lists', 'Graphs', 'Dijkstra\'s Algorithm', 'Topological Sort', 'Insertion Sort', 'Selection Sort', 'Treesort', 'Heapsort', 'Quicksort', 'Merge Sort', 'Counting Sort', 'Radix Sort', 'Union-Find']
+
 function Gameboard() {
-    return <div id="gameboard"></div>
+  return (
+    <section id="gameboard">
+        {cards.map((card, index) => <button key={index} type="button">{card}</button>)}
+    </section>
+  )
 }
 
 export default Gameboard
