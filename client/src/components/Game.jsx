e2842ea81f00aa4c8b4d365f61f5a931985cb722
kp-pham <170148118+kp-pham@users.noreply.github.com>
2026-01-03 09:23:07 -0800

    refactor: create component for game
    Players connect to game through matchmaking.

---
+++ client/src/components/Game.jsx
@@ -0,0 +1,13 @@
+import ChatPanel from './ChatPanel.jsx'
+import Gameboard from './Gameboard.jsx'
+
+function Game() {
+  return (
+    <main>
+      <Gameboard></Gameboard>
+      <ChatPanel></ChatPanel>
+    </main>
+  )
+}
+
+export default Game
