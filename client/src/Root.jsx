7c694d69b9510158c91625484ea1d445e352af86
kp-pham <170148118+kp-pham@users.noreply.github.com>
2026-01-04 14:02:34 -0800

    refactor: create custom CSS properties
    Reuse custom CSS properties to allow for modifications to cascade across components.

--- client/src/Root.jsx
+++ client/src/Root.jsx
@@ -2,7 +2,7 @@ import { isMobile } from 'react-device-detect'
 import { SocketProvider } from './contexts/index.jsx'
 import { MobileRedirect } from './components'
 import App from './App.jsx'
-import './index.css'
+import './styles/index.css'
 
 function Root() {
   if (isMobile)
